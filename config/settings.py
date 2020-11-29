8e91b11135ec9a8d1ccbd9cfaa546e6b551e6059
davis67 <davisag67@gmail.com>
2020-11-29 08:18:14 +0300

    Authentication (#1)
    * authentication set-up
    
    * posts set up

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # CUSTOM PROJECTS
+    'authentication.apps.AuthenticationConfig',
+    'posts.apps.PostsConfig',
+    'core.apps.CoreConfig'
 ]
 
 MIDDLEWARE = [
---
+++ posts/models.py
@@ -0,0 +1,61 @@
+from django.db import models
+from django.shortcuts import reverse
+from core import models as core_models
+
+
+class Category(core_models.TimeStampedModel):
+    """ Category Model Definition """
+
+    name = models.CharField(max_length=140)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.name
+
+
+class Comment(core_models.TimeStampedModel):
+    """ Comment Model Definition"""
+
+    description = models.TextField()
+    author = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    post = models.ForeignKey(
+        "Post", related_name="comments", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.description
+
+
+class Reply(core_models.TimeStampedModel):
+    """ Reply Model Definition"""
+
+    description = models.TextField()
+    comment = models.ForeignKey(
+        "Comment", related_name="replies", on_delete=models.CASCADE, blank=True, null=True)
+    author = models.ForeignKey("users.User", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.description
+
+
+class Post(core_models.TimeStampedModel):
+    """ Post Model Definition """
+
+    title = models.CharField(max_length=140)
+    description = models.TextField()
+    photo = models.ImageField(blank=True, upload_to='post_photos/')
+    author = models.ForeignKey(
+        "users.User", related_name="author", on_delete=models.CASCADE)
+    is_published = models.BooleanField(default=False)
+    authorized_by = models.ForeignKey(
+        "users.User", on_delete=models.SET_NULL, null=True, blank=True, default=None)
+    category = models.ForeignKey(
+        "Category", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def latest_post(self):
+        post, = Post.objects.all()[:1]
+        return post
